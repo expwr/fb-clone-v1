9fa5fb7412fdea0520c8b65acb6e6b3cd72fc962
expisexperience <noah.diana10312005@gmail.com>
2023-01-09 10:34:33 -0500

    working server

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-let allowed = ['http://localhost:3000', 'some other link']
-function options(res, req) {
-    let tmp;
-    let origin = req.header('Origin');
-    if(allowed.indexOf(origin) > -1) {
-        tmp = {
-            origin: true,
-            optionSuccessStatus: 200,
-        }
-    } else {
-        tmp = {
-            origin: 'stupid'
-        }
-    }
-    res(null, tmp);
+
+const options = {
+    origin: 'http://localhost:3000',
+    useSuccessStatus: 200,
 }
+// let allowed = ['http://localhost:3000', 'some other link']
+// function options(res, req) {
+//     let tmp;
+//     let origin = req.header('Origin');
+//     if(allowed.indexOf(origin) > -1) {
+//         tmp = {
+//             origin: true,
+//             optionSuccessStatus: 200,
+//         }
+//     } else {
+//         tmp = {
+//             origin: 'stupid'
+//         }
+//     }
+//     res(null, tmp);
+// }
 app.use(cors(options));
 
 app.get('/', (req, res) => {
-    res.send('Weolcome from home');
+    res.send('Welcome from home');
 })
 app.get('/books', (req, res) => {
     res.send('welcome from the new books homes');
