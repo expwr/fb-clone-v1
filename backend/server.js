7e0f2bbca0f55a13597b5a6b7ca14abbb9a39dfc
Noah Diana <noah.diana10312005@gmail.com>
2023-01-10 01:16:14 -0500

    Section 3, 22, 7:34 (not working won't post)

--- backend/server.js
+++ backend/server.js
@@ -1,36 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+
+
+const { readdirSync } = require('fs');
+
 const app = express();
+app.use(express.json())
+app.use(cors());
 
-const options = {
-    origin: 'http://localhost:3000',
-    useSuccessStatus: 200,
-}
-// let allowed = ['http://localhost:3000', 'some other link']
-// function options(res, req) {
-//     let tmp;
-//     let origin = req.header('Origin');
-//     if(allowed.indexOf(origin) > -1) {
-//         tmp = {
-//             origin: true,
-//             optionSuccessStatus: 200,
-//         }
-//     } else {
-//         tmp = {
-//             origin: 'stupid'
-//         }
-//     }
-//     res(null, tmp);
-// }
-app.use(cors(options));
 
-app.get('/', (req, res) => {
-    res.send('Welcome from home');
+// routes
+readdirSync('./routes').map((r) => app.use('/', require('./routes/' + r)));
+
+
+// database
+mongoose.connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
 })
-app.get('/books', (req, res) => {
-    res.send('welcome from the new books homes');
-})
+.then(() => console.log('database connected successfully'))
+.catch((err) => console.log('error connecting to mongodb', err))
 
-app.listen(8000, () => {
-    console.log("Server listening");
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 })
