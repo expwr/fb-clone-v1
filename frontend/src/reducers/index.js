c5d877cc245f41d313b684f8bc4991426d52af0d
JonFactor <Jon.Factor2@gmail.com>
2023-03-02 12:58:17 -0500

    opp

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -2,9 +2,9 @@ import { combineReducers } from "redux";
 import { themeReducer } from "./themeReducer";
 import { userReducer } from "./userReducer";
 
-const rootReducer = combineReducers({
-  user: userReducer,
-  darkTheme: themeReducer,
+const rootReducer = combineReducers({   // use the reducer from the files below and combine them (put togher)
+  user: userReducer,                    // set user to the userReducer fn output
+  darkTheme: themeReducer,              // set theme to themeReducers output
 });
 
-export default rootReducer;
+export default rootReducer;             // returning the reducers
--- frontend/src/routes/NotLoggedInRoutes.js
+++ frontend/src/routes/NotLoggedInRoutes.js
@@ -1,8 +1,8 @@
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
 
-export default function NotLoggedInRoutes() {
-  const { user } = useSelector((state) => ({ ...state }));
+export default function NotLoggedInRoutes() {                 // fn for not logged in user
+  const { user } = useSelector((state) => ({ ...state }));    // get user status as bool
 
-  return user ? <Navigate to="/" /> : <Outlet />;
+  return user ? <Navigate to="/" /> : <Outlet />;             // if user == true return element Navigate else outlet
 }
