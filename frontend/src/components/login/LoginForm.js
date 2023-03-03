8872efe7ab82e403007ec4a9987e0d7558c0e25e
ChaseHutcheson <ChaseTheGamer57@gmail.com>
2023-03-02 20:40:21 -0500

    Working on sidebar icons

--- frontend/src/components/login/LoginForm.js
+++ frontend/src/components/login/LoginForm.js
@@ -8,6 +8,7 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import Cookies from "js-cookie";
 import { useNavigate } from "react-router-dom";
+import logo from "../../assets/icons/Intwine-Logo-Full.png"
 
 const loginInfos = {
   email: "",
@@ -52,7 +53,7 @@ export default function LoginForm({ setVisible }) {
   return (
     <div className="login_wrap">
       <div className="login_1">
-        <img src="../../icons/facebook.svg" alt="" />
+        <img src={logo} alt="" id="Login-Logo"/>
         <span>
           Intwine helps you connect and share with the people in your life.
         </span>
